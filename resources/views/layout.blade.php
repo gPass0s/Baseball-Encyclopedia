03b56707b20a9674481fe8ea54852d401417f524
Guilherme Passos <guilhermepassos@outlook.com>
2019-03-24 08:41:48 -0300

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,35 +1,36 @@
 <!doctype html>
 <html lang="en-us">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<!-- CSRF Token -->
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+		<!-- CSRF Token -->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<title>Baseball Players Info</title>
+		<title>Baseball Players Info</title>
 
-	<!-- Styles -->
-	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+		<!-- Styles -->
+		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-	<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
-</head>
+		<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
+	</head>
 
-<body class="bg-light">
+	<body class="bg-light">
 
-	<header>
-		<nav> @yield('header') </nav>
-	</header>
+		<header>
+			<nav> @yield('header') </nav>
+		</header>
 
-	<div class="container">
+		<div class="container">
+			
+			@yield('content')
+
+			<footer class="my-5 pt-5 text-muted text-center text-small">
+				<p class="mb-1">&copy; By <a href=https://github.com/gPass0s> @pass0s</a></p>
+			</footer>
+			
+		</div>
+
 		
-		@yield('content')
-		
-	</div>
-
-	<footer class="my-5 pt-5 text-muted text-center text-small">
-		<p class="mb-1">&copy; By <a href=https://github.com/gPass0s> @gPass0s</a></p>
-	</footer>
-
-</body>
+	</body>
 </html>
