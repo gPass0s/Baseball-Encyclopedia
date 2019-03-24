8a5fbc146be2c348fb9d3f49ba72307d3492d3b6
Guilherme Passos <guilhermepassos@outlook.com>
2019-03-24 08:40:49 -0300

    Update player.blade.php

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -6,24 +6,25 @@
 
 @section('content')
 	<br>
-	<div>
+	<div align="center">
 		<div id="div_left">
-			<img src="{{$player->img_URL}}" width="120px" />
+			<img src="{{$player->img_URL}}" width="150px">
 		</div>
 		<br>
 		<div id="div_right">
 			<p> <b> Position(s): </b>{{$player->position}} </p>
 			<p> <b> Birthday: </b>{{$player->birthday}} </p>
 			<p> <b> Place of birth: </b>{{$player->birthday_place}} </p>
-		</div>
+
+			@php($names = explode(' ',$player->name))
+			@php($letter = substr(end($names),0,1))
+
+			<form action="/players/{{$letter}}">
+    			<input type="submit" value="Go back"/>
+			</form>
+		</div>	
 	</div>
 
-	@php($names = explode(' ',$player->name))
-	@php($letter = substr(end($names),0,1))
-
-	<form action="/players/{{$letter}}">
-    	<input type="submit" value="Go back"/>
-	</form>
 
 @endsection
 
